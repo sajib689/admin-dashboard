816d88c1d82426caed691fa795c31ceea4e3eaa7
sajib689 <jibonraja11@gmail.com>
2025-02-06 22:08:44 +0600

    fetch and display users from API in Users component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 
 
 const Users = () => {
+    const [users, setUsers] = useState([]);
+    useEffect( () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => setUsers(data))
+    },[])
     return (
         <div>
-            <h1>Hello</h1>
+            <h1 className="text-2xl font-bold text-center">Users</h1>
+            <ul>
+                {users.map(user => (
+                    <li key={user.id} className="flex items-center justify-between p-2 border-b">
+                        <span>{user.name}</span>
+                        <span>{user.email}</span>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
