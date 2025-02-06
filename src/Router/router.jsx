1fd03b29f91a0c911f579f4bcc4cf2aa8270830d
sajib689 <jibonraja11@gmail.com>
2025-02-06 22:20:54 +0600

    add route for ViewUser component to display user details

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Users from "../components/Users";
 import Products from "../components/Products";
+import ViewUser from "../components/ViewUser";
 
 
 const router = createBrowserRouter([
@@ -16,6 +17,11 @@ const router = createBrowserRouter([
            {
             path: '/products',
             element: <Products/>
+           },
+           {
+            path: '/users/:id',
+            element: <ViewUser/>,
+            loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/users/${param.id}`)
            }
         ]
     }
