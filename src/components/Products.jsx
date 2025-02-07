90a5dcbc7c1ce968dee4cfd60e4f36916c60cdf0
sajib689 <jibonraja11@gmail.com>
2025-02-07 10:11:27 +0600

    add loading state and enhance Products component with responsive table and detail links

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const Products = () => {
@@ -14,10 +15,42 @@ const Products = () => {
           })
           .catch((error) => console.error("Error fetching data:", error));
       }, []);
+      if (loading) return <h1 className="text-center text-xl">Loading...</h1>;
     return (
-        <div>
-            
+        <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold text-left mb-6">
+          Total Products: {products?.length}
+        </h1>
+        <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+          <table className="min-w-full table-auto">
+            <thead className="bg-gradient-to-r from-blue-600 to-purple-600 text-white">
+              <tr>
+                <th className="px-6 py-3 text-left font-medium">Name</th>
+                <th className="px-6 py-3 text-left font-medium">Email</th>
+                <th className="px-6 py-3 text-left font-medium">City</th>
+                <th className="px-6 py-3 text-left font-medium">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products?.map((user) => (
+                <tr key={user.id} className="border-t hover:bg-gray-100">
+                  <td className="px-6 py-4">{user.name}</td>
+                  <td className="px-6 py-4">{user.email}</td>
+                  <td className="px-6 py-4">{user.address.city}</td>
+                  <td className="px-6 py-4">
+                    <Link
+                      to={`/users/${user?.id}`}
+                      className="bg-gradient-to-r from-green-400 to-teal-500 text-white px-3 py-2 md:px-4 md:py-2 rounded text-sm md:text-base w-full md:w-auto text-center block md:inline-block"
+                    >
+                      View Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     );
 };
 
