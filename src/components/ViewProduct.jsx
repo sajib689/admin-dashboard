42948c22ad9f752ec0aedff65b09d309b29ac7ec
sajib689 <jibonraja11@gmail.com>
2025-02-07 10:40:55 +0600

    add back navigation button to ViewProduct component

--- src/components/ViewProduct.jsx
+++ src/components/ViewProduct.jsx
@@ -1,10 +1,16 @@
-import { Link, useLoaderData } from "react-router-dom";
-
+import { Link, useLoaderData, useNavigate } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 const ViewProduct = () => {
   const product = useLoaderData();
-
+    const navigate = useNavigate()
   return (
     <div className="max-w-lg mx-auto p-6 bg-white shadow-lg rounded-lg mt-10">
+      <button 
+      onClick={() => navigate(-1)} 
+      className="flex items-center gap-2 bg-gradient-to-r from-gray-700 to-gray-900 text-white px-4 py-2 rounded-md hover:from-gray-600 hover:to-gray-800 transition-all duration-300"
+    >
+      <ArrowLeft size={20} /> Back
+    </button>
       <h2 className="text-2xl font-bold text-gray-800 mb-2">{product?.name}</h2>
 
       <div className="border-t border-gray-300 mt-2 pt-4">
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/router.jsx'
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}>
-   
+   <Toaster/>
   </RouterProvider>,
 )
